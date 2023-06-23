fc64d917e8d5e0daf35063c70d115b35971a4963
Jose Alberto Borrego <50537384+Josbor@users.noreply.github.com>
2023-06-22 21:25:16 -0400

    fix(mx-scanner): exports of components in index.ts file (#15)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
+export { MxScannerDialog } from './lib/components/mx-scanner/mx-scanner.dialog';
+export { MxScannerComponent } from './lib/mx-scanner.component';
 export * from './lib/mx-scanner.module';
