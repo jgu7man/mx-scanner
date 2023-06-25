33a8fe813395768b36101c0120ec8b21d0f13c02
Jorge Gu7man <46007655+jgu7man@users.noreply.github.com>
2023-06-25 08:01:02 -0600

    fix(pdv): fix imports (#37)
    - Using the MxFire library
    - Formating devkit/cache and devkit/text files
    - Exporting models from scanner library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { MxScannerDialog } from './lib/components/mx-scanner/mx-scanner.dialog';
 export { MxScannerComponent } from './lib/mx-scanner.component';
+export * from './lib/mx-scanner.model';
 export * from './lib/mx-scanner.module';
+export * from './lib/mx-scanner.service';
